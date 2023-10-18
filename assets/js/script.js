3897a76ce3ac64fa6dc8269e92841ebf07df921a
Vigneswari Sambandam <vigneswari@Vigneswaris-MacBook-Air.local>
2023-10-18 14:29:22 -0400

    Added dummy section for questions

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Sections should be displayed and hidden as necessary - Create a function for it
+// Create a function to populate questions
+// Create a function to check the selected answer
+// Create a function to show the answer
+// A function for timer to be created.
